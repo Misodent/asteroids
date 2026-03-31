bd7acbb2f634699334e87826cd536c50451e17a7
Misodent <email@malerch.com>
2026-03-31 23:44:43 +0200

    fps handling

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -19,8 +21,10 @@ def main():
                 return
 
         screen.fill("black")
-
         pygame.display.flip()
+
+        tick = clock.tick(60)
+        dt = tick / 1000
 
 if __name__ == "__main__":
     main()
